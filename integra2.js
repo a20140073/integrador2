9749931a3e4b82e620efc289a9ccf825d918e5dc
a20140073 <131172847+a20140073@users.noreply.github.com>
2023-09-19 08:40:41 -0500

    Add files via upload

---
+++ integra2.js
@@ -0,0 +1,23 @@
+var nom=prompt("Ingrese su nombre");
+
+var preg1=prompt("¿Cómo se llama el 1er día de primavera?\nA:Primer día de primavera\nB:Equinoccio de primavera\nC:Equinoccio de invierno");
+if (preg1=="B"){
+    preg1=25
+}
+else{
+    preg1=0 
+}
+var preg2=prompt("¿Qué sucede en el Polo Norte cuando llega la Primavera?\nA:6 meses de luz diurna ininterrumpida\nB:6 meses de oscuridad ininterrumpida\nC:6 meses de alternancia de luz y oscuridad");
+if (preg2=="A"){
+    preg2=25
+}
+else{
+    preg2=0 
+}
+var total=preg1+preg2
+if (total>25){
+    document.write(nom, " sabes de primavera")
+}
+else{
+    document.write (nom, " te falta")
+}
